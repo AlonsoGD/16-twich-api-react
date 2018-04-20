1abdb84bea22eb63a17f5037d4f8ac1f47e3f765
Daniel Alonso <dani.alonso.garcia@gmail.com>
2018-04-20 00:20:24 -0400

    test

--- script.js
+++ script.js
@@ -6,13 +6,20 @@ let streamsData = []
 
 const MAINDIV = document.getElementById("main");
 
-const DOM_OBJECT = {
-  cimage: document.getElementsByClassName("cimage"),
-  ctitle: document.getElementsByClassName("ctitle"),
-  clink: document.getElementsByClassName("clink"),
-  cstatus: document.getElementsByClassName("cstatus"),
-  cviewers: document.getElementsByClassName("cviewers"),
-  cgame: document.getElementsByClassName("cgame")
+function showInfo () {
+  for (let i = 1; i < streamsData.length; i++) {
+    if (streamsData[i].stream !== null) {
+      MAINDIV.innerHTML += 
+        "<ul><li>" 
+        + usersData[i].logo + "<li>"
+        + usersData[i].display_name + "<li>"
+        + streamsData[i].stream._id + "<li>"
+        + streamsData[i].stream.viewers + "<li>"
+        + streamsData[i].stream.game + ""
+        + "</lu>";
+        console.log("TST");
+    } 
+  }
 }
 
 function callTwichAPI () {
@@ -25,10 +32,16 @@ function callTwichAPI () {
         return response.json();
       })
       .then(function(response) {
-        console.log('Request successful');
+        console.log('Request successful', response.display_name);
+          MAINDIV.innerHTML +=
+          "<ul><li>" 
+          + response.logo + "<li>"
+          + response.display_name
+          + "</lu>";
         usersData.push(response)
-        showInfo();
       })
+      
+
       .catch(function(error) {
         console.log('Request failed', error)
     });
@@ -39,7 +52,15 @@ function callTwichAPI () {
       })
       .then(function(response) {
         console.log('Request successful'); 
-        streamsData.push(response);
+        if (response.stream !== null) {
+          MAINDIV.innerHTML +=
+          "<ul><li>" 
+          + response.stream._id + "<li>"
+          + response.stream.viewers + "<li>"
+          + response.stream.game + ""
+          + "</lu>";
+          streamsData.push(response);
+        }
       })
       .catch(function(error) {
         console.log('Request failed', error)
@@ -47,28 +68,4 @@ function callTwichAPI () {
   }
 }
 
-function showInfo () {
-  for (let i = 1; i < 5; i++) {
-    if (streamsData[i].stream !== null) {
-      MAINDIV.innerHTML += 
-        "<div>" 
-        + usersData[i].logo + " "
-        + usersData[i].display_name + " "
-        + streamsData[i].stream._id + " "
-        + streamsData[i].stream.viewers + " "
-        + streamsData[i].stream.game + " "
-        + "</div>";
-    }
-    // DOM_OBJECT.ctitle[i].textContent = usersData[i].display_name;
-    // DOM_OBJECT.cstatus[i].innerHTML = streamsData[i].stream._id;
-    // DOM_OBJECT.cviewers[i].innerHTML = streamsData[i].stream.viewers;
-    // DOM_OBJECT.cgame[i].innerHTML = streamsData[i].stream.game; 
-  }
-}
-function main () {
-  callTwichAPI();
-  
-}
-
-
-main();
+callTwichAPI();
