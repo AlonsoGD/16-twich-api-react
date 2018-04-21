c781de00a1b494c1948134b4bcf0e9831b320180
Daniel Alonso <dani.alonso.garcia@gmail.com>
2018-04-21 13:43:08 -0400

    refactored js, finished project

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-
-//const CALLS = ['users/', 'streams/']
-
 let usersData = []
 let streamsData = []
 
 const MAINDIV = document.getElementById("main");
 
-function callTwichAPI () {
+const showOnlineChannels = function (r) {
+  let address = "https://www.twitch.tv/" + r.stream.channel.display_name;
+  MAINDIV.innerHTML +=
+  "<ul class='online'><li onClick='window.open(\"" + address + "\", \"_blank\")'>"
+    + "<div class='imgContainer'><img class='profileImg' src='" 
+      + r.stream.channel.logo + "'></div>" + "<div class='linkContainer'><a href='" + address + "'>" + r.stream.channel.display_name 
+    + "</a></div></li>" 
+    + "<li>" + "Viewers: " + r.stream.viewers + "</li>" 
+    + "<li>" + "Currently streaming: " + r.stream.game + "</li>"
+  + "</lu>";
+};
+
+const showOfflineChannles = function (r) {
+  let address = "https://www.twitch.tv/" + r.display_name;
+  MAINDIV.innerHTML +=
+  "<ul class='offline'><li onClick='window.open(\"" + address + "\", \"_blank\")'>"
+    + "<div class='imgContainer'><img class='profileImg' src='" 
+      + r.logo + "'></div>" + "<div class='linkContainer'><a href='" + address + "'>" + r.display_name 
+    + "</a></div></li>" 
+  + "</lu>";
+}
+
+const callTwichApi = function () {
   const URL = 'https://wind-bow.glitch.me/twitch-api/'
   const CHANNELS = ['tsm_myth', 'FreeCodeCamp', 'shroud', 'OgamingSC2', 'cretetion', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas']
 
@@ -18,13 +37,7 @@ function callTwichAPI () {
       .then(function(response) {
         console.log('Request successful'); 
         if (response.stream !== null) {
-          MAINDIV.innerHTML +=
-          "<ul><li>" 
-          + "<img class='profileImg' src='" + response.stream.channel.logo + "'>" + "<li>"
-          + "<a href='https://www.twitch.tv/" + response.stream.channel.display_name + "'>" + response.stream.channel.display_name + "</a><li>"
-          + "Viewers: " + response.stream.viewers + "<li>"
-          + "Currently streaming: " + response.stream.game + ""
-          + "</lu>";
+          showOnlineChannels(response);
           streamsData.push(response);
         } else {
           fetch(URL + 'users/' + CHANNELS[i])
@@ -32,12 +45,7 @@ function callTwichAPI () {
               return response.json();
             })
             .then(function(response) {
-              console.log('Request successful', response.display_name);
-                MAINDIV.innerHTML +=
-                "<ul><li>" 
-                + "<img class='profileImg' src='" + response.logo + "'>" + "<li>"
-                + "<a href='https://www.twitch.tv/" + response.display_name + "'>" + response.display_name + "</a></li>"
-                + "</lu>";
+              showOfflineChannles(response);
               usersData.push(response)
             })    
             .catch(function(error) {
@@ -49,6 +57,6 @@ function callTwichAPI () {
         console.log('Request failed', error)
     });
   }
-}
+};
 
-callTwichAPI();
+callTwichApi();
