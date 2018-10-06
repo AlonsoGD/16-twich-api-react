f151961164fcd5ab5fad7e7b238dc357ee4d519f
AlonsoGD <dani.alonso.garcia@gmail.com>
2018-10-05 21:19:56 -0400

    Refactor with ES6 Syntax

--- script.js
+++ script.js
@@ -3,26 +3,45 @@ let streamsData = []
 
 const MAINDIV = document.getElementById("main");
 
-const showOnlineChannels = function (r) {
+const showOnlineChannels = r => {
   let address = "https://www.twitch.tv/" + r.stream.channel.display_name;
+  // New ES6 Syntax
   MAINDIV.innerHTML +=
-  "<ul class='online'><li onClick='window.open(\"" + address + "\", \"_blank\")'>"
-    + "<div class='imgContainer'><img class='profileImg' src='" 
-      + r.stream.channel.logo + "'></div>" + "<div class='linkContainer'><a href='" + address + "'>" + r.stream.channel.display_name 
-    + "</a></div></li>" 
-    + "<li>" + "Viewers: " + r.stream.viewers.toLocaleString(undefined, { minimumFractionDigits: 0}) + "</li>" 
-    + "<li>" + "Currently streaming: " + r.stream.game + "</li>"
-  + "</lu>";
+  `<ul class='online'>
+    <li onClick='window.open("${address}", "_blank")'>
+      <div class='imgContainer'><img class='profileImg' src='${r.stream.channel.logo}'></div>
+      <div class='linkContainer'><a href='${address}'>${r.stream.channel.display_name}</a></div>
+    </li>
+    <li>Viewers: ${r.stream.viewers.toLocaleString(undefined, { minimumFractionDigits: 0})}</li>
+    <li>Currently streaming: ${r.stream.game}</li>
+  </ul>`
+
+
+  // MAINDIV.innerHTML +=
+  // "<ul class='online'><li onClick='window.open(\"" + address + "\", \"_blank\")'>"
+  //   + "<div class='imgContainer'><img class='profileImg' src='" 
+  //     + r.stream.channel.logo + "'></div>" + "<div class='linkContainer'><a href='" + address + "'>" + r.stream.channel.display_name 
+  //   + "</a></div></li>" 
+  //   + "<li>" + "Viewers: " + r.stream.viewers.toLocaleString(undefined, { minimumFractionDigits: 0}) + "</li>" 
+  //   + "<li>" + "Currently streaming: " + r.stream.game + "</li>"
+  // + "</ul>";
 };
 
-const showOfflineChannles = function (r) {
+const showOfflineChannles = r => {
   let address = "https://www.twitch.tv/" + r.display_name;
-  MAINDIV.innerHTML +=
-  "<ul class='offline'><li onClick='window.open(\"" + address + "\", \"_blank\")'>"
-    + "<div class='imgContainer'><img class='profileImg' src='" 
-      + r.logo + "'></div>" + "<div class='linkContainer'><a href='" + address + "'>" + r.display_name 
-    + "</a></div></li>" 
-  + "</lu>";
+  
+  MAINDIV.innerHTML += 
+    `<ul class='offline'><li onClick='window.open("${address}", "_blank")'>
+        <div class='imgContainer'><img class='profileImg' src='${r.logo}'></div>
+        <div class='linkContainer'><a href='${address}'>${r.display_name}</a></div></li>
+    </ul>`
+  
+  // MAINDIV.innerHTML +=
+  // "<ul class='offline'><li onClick='window.open(\"" + address + "\", \"_blank\")'>"
+    // + "<div class='imgContainer'><img class='profileImg' src='" 
+      // + r.logo + "'></div>" + "<div class='linkContainer'><a href='" + address + "'>" + r.display_name 
+    // + "</a></div></li>" 
+  // + "</ul>";
 }
 
 const callTwichApi = function () {
@@ -31,31 +50,23 @@ const callTwichApi = function () {
 
   for (let i = 0; i < CHANNELS.length; i++ ) {
     fetch(URL + 'streams/' + CHANNELS[i])
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(response) {
+      .then(response => response.json())
+      .then(response => {
         console.log('Request successful'); 
         if (response.stream !== null) {
           showOnlineChannels(response);
           streamsData.push(response);
         } else {
           fetch(URL + 'users/' + CHANNELS[i])
-            .then(function(response) {
-              return response.json();
-            })
-            .then(function(response) {
+            .then(response => response.json())
+            .then(response => {
               showOfflineChannles(response);
               streamsData.push(response)
             })    
-            .catch(function(error) {
-              console.log('Request failed', error)
-            });
+            .catch(error => console.log('Request failed', error));
         }
       })
-      .catch(function(error) {
-        console.log('Request failed', error)
-    });
+      .catch(error => console.log('Request failed', error));
   }
 };
 
