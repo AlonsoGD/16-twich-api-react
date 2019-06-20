946202a6102be4ff3e565623acc71ddc7913aad2
AlonsoGD <dani.alonso.garcia@gmail.com>
2019-06-19 22:58:52 -0400

    Updates

---
+++ precache-manifest.f270f06552efc0236b235bbd9f579479.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76682b631316f72fc64c7eecbeb80741",
+    "url": "/16-twich-api-react/index.html"
+  },
+  {
+    "revision": "9db3a0db05a9190684e7",
+    "url": "/16-twich-api-react/static/css/main.8b2326ae.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/16-twich-api-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9db3a0db05a9190684e7",
+    "url": "/16-twich-api-react/static/js/main.5a4cb45f.chunk.js"
+  },
+  {
+    "revision": "3e865d1c35c3582a5fe8",
+    "url": "/16-twich-api-react/static/js/runtime~main.d6ec1a65.js"
+  }
+]);
